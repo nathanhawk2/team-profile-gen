09e626f82390135881cc35ae272b3c19058cfc02
nathanhawk2 <ntrumpet620@gmail.com>
2022-09-26 18:24:48 -0400

    finished lib pages, working on index.js

--- index.js
+++ index.js
@@ -3,3 +3,39 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
 // TODO: CODE GOES HERE
+const inquirer = require('inquirer');
+const chalk = require("chalk");
+const path = require('path');
+const fs = require('fs');
+const generateTeam = require('./src/page-template.js');
+const { default: inquirer } = require('inquirer');
+
+team = [];
+
+function buildTeam() {
+    inquirer.prompt([{
+        type: 'list',
+        message: 'What role would you like to add?',
+        name: 'addEmployee',
+        choices: [
+            'Manager',
+            'Engineer',
+            'Intern',
+            'Do not add any more team members'
+        ]
+    }]).then(function (userInput) {
+        switch (userInput.addEmployee) {
+            case 'Manager':
+                addManager();
+                break;
+            case 'Engineer':
+                addEngineer();
+                break;
+            case 'Intern':
+                addIntern();
+                break;
+            default:
+                htmlBuilder();
+        }
+    })
+}
